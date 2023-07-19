8945bce9485afec81da197225e279844ed8c1c61
taqiniazi <taqiniazi61@gmail.com>
2023-07-19 12:44:45 +0500

    my Updates

---
+++ myfile.js
@@ -0,0 +1,20 @@
+"use strict";
+let firstname = "Taqi";
+let secondname = "Raza";
+let age = 32;
+let isMarried = false;
+if (age >= 35) {
+    console.log(firstname + " " + secondname + " is Matured");
+}
+else if (age <= 34 && age >= 22) {
+    console.log(firstname + " " + secondname + " is Energatic");
+}
+else {
+    console.log(firstname + " " + secondname + " is too Young");
+}
+if (isMarried) {
+    console.log(firstname + " " + secondname + " is Married");
+}
+else {
+    console.log(firstname + " " + secondname + " is not Married");
+}
---
+++ myfile.ts
@@ -0,0 +1,18 @@
+let firstname : string = "Taqi"
+let secondname : string = "Raza"
+let age : number = 32
+let isMarried : boolean = false;
+
+if(age>=35){
+    console.log(firstname + " " + secondname + " is Matured")
+}else if(age<=34 && age>=22){
+    console.log(firstname + " " + secondname + " is Energatic")
+}else{
+    console.log(firstname + " " + secondname + " is too Young")
+}
+
+if(isMarried){
+    console.log(firstname + " " + secondname + " is Married")
+}else{
+    console.log(firstname + " " + secondname + " is not Married")
+}
