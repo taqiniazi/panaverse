2f820b113a89c28ea68bc3a7bff94b1873b1ee15
taqiniazi <taqiniazi61@gmail.com>
2023-08-07 23:00:26 +0500

    tsccccc

--- Assignment/veriables.js
+++ Assignment/veriables.js
@@ -1,7 +1,13 @@
 "use strict";
-let pets = [];
-pets[0] = "dog";
-pets[1] = "cat";
-pets.pop();
-pets.push("parrot");
-console.log(pets);
+// let pets:any = [];
+// pets[0] = "dog"  ; 
+// pets[1] = "cat"  ;
+// pets.pop();
+// pets.push("parrot")
+// console.log(pets)
+// let pets:any = ["dog","cat","hen","peacock"];
+// pets.splice(1,1,"Pet1", "Pet2")
+// console.log(pets);  
+let pets = ["dog", "cat", "hen", "peacock"];
+const newpets = pets.slice(1);
+console.log(newpets);
