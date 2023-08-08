3f395865ffd6c59b503a31a1b64616abbd0308de
taqiniazi <taqiniazi61@gmail.com>
2023-08-08 11:54:33 +0500

    Updated

--- Assignment/Voting/voting.ts
+++ Assignment/Voting/voting.ts
@@ -1,11 +1,12 @@
+// install the promptSync package first to get input
+// npm install prompt-sync
+
+
 import prompSync from 'prompt-sync'
 const prompt = prompSync()
 let personAge:any = prompt("What is your age")
-// console.log(`Hello, Your age is ${personAge}`)
-// let personAge = 17;
 if(personAge>=18){
     console.log("You are eligibale to Vote")
 }else{
     console.log("You are not eligibale to Vote")
 }
-// export{}
